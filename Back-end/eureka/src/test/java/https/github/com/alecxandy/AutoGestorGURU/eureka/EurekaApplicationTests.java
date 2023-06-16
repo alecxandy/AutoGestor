d6dfd4d4421f65222da7c801d290808efd46c242
alecxandy <alecxandy@gmail.com>
2023-06-16 00:02:32 -0300

    Iniciando o service discovery

---
+++ Back-end/eureka/src/test/java/https/github/com/alecxandy/AutoGestorGURU/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package https.github.com.alecxandy.AutoGestorGURU.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

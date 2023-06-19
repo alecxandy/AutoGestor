62d458593afb2f5964f0779a339ec8a692cf860d
alecxandy <alecxandy@gmail.com>
2023-06-19 20:17:52 -0300

    configuração de rotas do gatway

--- Back-end/gateway/src/main/java/https/github/com/alecxandy/AutoGestorGURU/gateway/GatewayApplication.java
+++ Back-end/gateway/src/main/java/https/github/com/alecxandy/AutoGestorGURU/gateway/GatewayApplication.java
@@ -3,6 +3,9 @@ package https.github.com.alecxandy.AutoGestorGURU.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -12,4 +15,11 @@ public class GatewayApplication {
 		SpringApplication.run(GatewayApplication.class, args);
 	}
 
+	@Bean
+	public RouteLocator routes(RouteLocatorBuilder routeLocatorBuilder) {
+		return routeLocatorBuilder
+				.routes()
+				.route(r -> r.path("/teacher/**").uri("lb://ms_teacher"))
+				.build();
+	}
 }

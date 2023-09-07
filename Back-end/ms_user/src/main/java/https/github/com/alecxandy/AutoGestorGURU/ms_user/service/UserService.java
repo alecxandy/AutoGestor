a771aee579508a8312c875d580c76b07704a2c9a
alecxandy <alecxandy@gmail.com>
2023-09-07 16:12:57 -0300

    Adicionando boas praticasa classe User @Trasactonal

--- Back-end/ms_user/src/main/java/https/github/com/alecxandy/AutoGestorGURU/ms_user/service/UserService.java
+++ Back-end/ms_user/src/main/java/https/github/com/alecxandy/AutoGestorGURU/ms_user/service/UserService.java
@@ -7,9 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
-
-import java.util.Optional;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class UserService {
@@ -21,14 +19,17 @@ public class UserService {
         return userRepository.save(user);
     }
 
+    @Transactional
     public Page<User> findAll(Pageable pageable) {
         return userRepository.findAll(pageable);
     }
 
+    @Transactional
     public User findById(Long id) {
         return userRepository.findById(id).orElseThrow(() -> new IdentifierNotFoundException());
     }
 
+    @Transactional
     public User update(User user) {
         return userRepository.findById(user.getId()).map(u -> {
             userRepository.save(user);
@@ -36,6 +37,7 @@ public class UserService {
         }).orElseThrow(() -> new IdentifierNotFoundException());
     }
 
+    @Transactional
     public void deleteById(Long id) {
         userRepository.findById(id).map(user -> {
             userRepository.deleteById(id);
@@ -43,6 +45,7 @@ public class UserService {
         }).orElseThrow(() -> new IdentifierNotFoundException());
     }
 
+    @Transactional
     public User findByCpf(String cpf) {
         return userRepository.findByCpf(cpf).orElseThrow(() -> new IdentifierNotFoundException());
     }

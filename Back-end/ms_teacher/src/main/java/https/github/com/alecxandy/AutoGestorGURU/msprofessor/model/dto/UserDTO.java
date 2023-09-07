e1dcb6d84e9411c12d1140bb63917ea87981cb97
alecxandy <alecxandy@gmail.com>
2023-09-07 16:24:25 -0300

    Adicionando classe de comunicação student/user

---
+++ Back-end/ms_teacher/src/main/java/https/github/com/alecxandy/AutoGestorGURU/msprofessor/model/dto/UserDTO.java
@@ -0,0 +1,32 @@
+package https.github.com.alecxandy.AutoGestorGURU.msprofessor.model.dto;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.br.CPF;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tb_user")
+public class UserDTO {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NotBlank(message = "Please fill in the 'name' field correctly")
+    private String name;
+
+    @NotBlank(message = "Please fill in the 'CPF' field correctly")
+    @CPF(message = "CPF invalid")
+    private String cpf;
+
+    @NotBlank(message = "Please fill in the 'password' field correctly")
+    private String password;
+
+}

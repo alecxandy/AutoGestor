3884fcbfd731f89e43971173f37b165ca1dc3782
alecxandy <alecxandy@gmail.com>
2023-09-12 22:36:27 -0300

    Dependencia para dar opcao de retorno com xml ou json

--- Back-end/ms_teacher/src/main/java/https/github/com/alecxandy/AutoGestorGURU/msprofessor/MsteacherApplication.java
+++ Back-end/ms_teacher/src/main/java/https/github/com/alecxandy/AutoGestorGURU/msprofessor/MsteacherApplication.java
@@ -3,7 +3,6 @@ package https.github.com.alecxandy.AutoGestorGURU.msprofessor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-
 @SpringBootApplication
 @EnableFeignClients
 public class MsteacherApplication {

c76b363b880e2fa2cb8c5cc5142dc7e525f6a46d
alecxandy <alecxandy@gmail.com>
2023-10-15 13:51:05 -0300

    implementando methods studentService(crud)

--- Back-end/ms_student/src/main/java/https/github/com/alecxandy/AutoGestorGURU/ms_student/repository/StudentRepository.java
+++ Back-end/ms_student/src/main/java/https/github/com/alecxandy/AutoGestorGURU/ms_student/repository/StudentRepository.java
@@ -4,6 +4,9 @@ import https.github.com.alecxandy.AutoGestorGURU.ms_student.model.entity.Student
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
+    Optional<Student> findByCpf(String cpf);
 }
--- Back-end/ms_student/src/main/java/https/github/com/alecxandy/AutoGestorGURU/ms_student/service/StudentService.java
+++ Back-end/ms_student/src/main/java/https/github/com/alecxandy/AutoGestorGURU/ms_student/service/StudentService.java
@@ -1,9 +1,13 @@
 package https.github.com.alecxandy.AutoGestorGURU.ms_student.service;
 
+import https.github.com.alecxandy.AutoGestorGURU.ms_student.exception.IdentifierNotFoundException;
 import https.github.com.alecxandy.AutoGestorGURU.ms_student.model.entity.Student;
 import https.github.com.alecxandy.AutoGestorGURU.ms_student.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class StudentService {
@@ -11,8 +15,41 @@ public class StudentService {
     @Autowired
     private StudentRepository studentRepository;
 
+    @Transactional
     public Student save(Student student) {
         return studentRepository.save(student);
+    }
+
+
+    @Transactional
+    public Page<Student> findAll(Pageable pageable) {
+        return studentRepository.findAll(pageable);
+    }
+
+    @Transactional
+    public Student findById(Long id) {
+        return studentRepository.findById(id).orElseThrow(() -> new IdentifierNotFoundException());
+    }
+
+    @Transactional
+    public Student update(Student student) {
+        return studentRepository.findById(student.getId()).map(e -> {
+            studentRepository.save(student);
+            return e;
+        }).orElseThrow(() -> new IdentifierNotFoundException());
+    }
+
+    @Transactional
+    public void deleteById(Long id) {
+        studentRepository.findById(id).map(e -> {
+            studentRepository.deleteById(id);
+            return e;
+        }).orElseThrow(() -> new IdentifierNotFoundException());
+    }
+
+    @Transactional
+    public Student findByCpf(String cpf) {
+        return studentRepository.findByCpf(cpf).orElseThrow(() -> new IdentifierNotFoundException());
     }
 
 

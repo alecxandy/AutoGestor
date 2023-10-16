4d89ff1f77150a352d95f6428617e526807b9d66
alecxandy <alecxandy@gmail.com>
2023-10-16 10:15:14 -0300

    Retirando codigo de comunição sicrona com feing

--- Back-end/ms_student/src/main/java/https/github/com/alecxandy/AutoGestorGURU/ms_student/infra/UserResourceClient.java
+++
@@ -1,9 +0,0 @@
-package https.github.com.alecxandy.AutoGestorGURU.ms_student.infra;
-
-import org.springframework.cloud.openfeign.FeignClient;
-
-@FeignClient(value = "msuser", path = "/user")
-public interface UserResourceClient {
-
-
-}

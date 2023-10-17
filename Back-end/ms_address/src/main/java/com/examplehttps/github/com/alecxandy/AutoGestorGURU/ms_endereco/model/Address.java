2dcf212265fe0b6e9fb9fffad34265fb89129c66
alecxandy <alecxandy@gmail.com>
2023-10-16 22:40:14 -0300

    Adicionando retorno do json em portugues

--- Back-end/ms_address/src/main/java/com/examplehttps/github/com/alecxandy/AutoGestorGURU/ms_endereco/model/Address.java
+++ Back-end/ms_address/src/main/java/com/examplehttps/github/com/alecxandy/AutoGestorGURU/ms_endereco/model/Address.java
@@ -1,5 +1,6 @@
 package com.examplehttps.github.com.alecxandy.AutoGestorGURU.ms_endereco.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -17,21 +18,29 @@ public class Address {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+
+    @JsonProperty(namespace = "Pais")
     @NotNull(message = "value cannot be null")
     private String country;
 
+    @JsonProperty(namespace = "Estado")
     @NotNull(message = "value cannot be null")
     private String state;
 
+    @JsonProperty(namespace = "Cidade")
     @NotNull(message = "value cannot be null")
     private String city;
 
+    @JsonProperty(namespace = "Bairro")
     @NotNull(message = "value cannot be null")
     private String district;
 
+
+    @JsonProperty(namespace = "Rua")
     @NotNull(message = "value cannot be null")
     private String road;
 
+    @JsonProperty(namespace = "CEP")
     @NotNull(message = "value cannot be null")
     private String zipCode;
 

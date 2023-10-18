238e16123e205f3f74c9bbcb6cce69a99a34f295
alecxandy <alecxandy@gmail.com>
2023-10-17 21:17:40 -0300

    iniciando microsservicos de Company

---
+++ Back-end/ms_company/src/main/java/https/github/com/alecxandy/AutoGestor/ms_company/MsCompanyApplication.java
@@ -0,0 +1,13 @@
+package https.github.com.alecxandy.AutoGestor.ms_company;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsCompanyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsCompanyApplication.class, args);
+	}
+
+}
---
+++ Back-end/ms_company/src/test/java/https/github/com/alecxandy/AutoGestor/ms_company/MsCompanyApplicationTests.java
@@ -0,0 +1,13 @@
+package https.github.com.alecxandy.AutoGestor.ms_company;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsCompanyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

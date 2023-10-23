6fe6d7e8fb87aa2b489b9140cddab3899b57c74d
alecxandy <alecxandy@gmail.com>
2023-10-23 07:07:05 -0300

    Service de Company e Exception

--- Back-end/ms_company/src/main/java/https/github/com/alecxandy/AutoGestor/ms_company/repository/CompanyRepository.java
+++ Back-end/ms_company/src/main/java/https/github/com/alecxandy/AutoGestor/ms_company/repository/CompanyRepository.java
@@ -4,6 +4,9 @@ import https.github.com.alecxandy.AutoGestor.ms_company.model.Company;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CompanyRepository extends JpaRepository<Company, Long> {
+    Optional<Company> findByCnpj(String cpf);
 }
---
+++ Back-end/ms_company/src/main/java/https/github/com/alecxandy/AutoGestor/ms_company/service/CompanyService.java
@@ -0,0 +1,55 @@
+package https.github.com.alecxandy.AutoGestor.ms_company.service;
+
+import https.github.com.alecxandy.AutoGestor.ms_company.exception.IdentifierNotFoundException;
+import https.github.com.alecxandy.AutoGestor.ms_company.model.Company;
+import https.github.com.alecxandy.AutoGestor.ms_company.repository.CompanyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class CompanyService {
+
+    @Autowired
+    private CompanyRepository companyRepository;
+
+
+    public Company save(Company company) {
+        return companyRepository.save(company);
+    }
+
+    @Transactional
+    public Page<Company> findAll(Pageable pageable) {
+        return companyRepository.findAll(pageable);
+    }
+
+    @Transactional
+    public Company findById(Long id) {
+        return companyRepository.findById(id).orElseThrow(() -> new IdentifierNotFoundException());
+    }
+
+    @Transactional
+    public Company update(Company company) {
+        return companyRepository.findById(company.getId()).map(u -> {
+            companyRepository.save(company);
+            return u;
+        }).orElseThrow(() -> new IdentifierNotFoundException());
+    }
+
+    @Transactional
+    public void deleteById(Long id) {
+        companyRepository.findById(id).map(user -> {
+            companyRepository.deleteById(id);
+            return user;
+        }).orElseThrow(() -> new IdentifierNotFoundException());
+    }
+
+    @Transactional
+    public Company findByCpf(String cpf) {
+        return companyRepository.findByCnpj(cpf).orElseThrow(() -> new IdentifierNotFoundException());
+    }
+
+
+}

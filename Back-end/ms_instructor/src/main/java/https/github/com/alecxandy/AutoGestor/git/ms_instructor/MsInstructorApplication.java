457f7a5ffd83d8144373f45eb5c8df7f05bdcd34
alecxandy <alecxandy@gmail.com>
2023-10-24 23:17:05 -0300

    Microsservicos de instrutores

---
+++ Back-end/ms_instructor/src/main/java/https/github/com/alecxandy/AutoGestor/git/ms_instructor/MsInstructorApplication.java
@@ -0,0 +1,13 @@
+package https.github.com.alecxandy.AutoGestor.git.ms_instructor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsInstructorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsInstructorApplication.class, args);
+	}
+
+}
---
+++ Back-end/ms_instructor/src/test/java/https/github/com/alecxandy/AutoGestor/git/ms_instructor/MsInstructorApplicationTests.java
@@ -0,0 +1,13 @@
+package https.github.com.alecxandy.AutoGestor.git.ms_instructor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsInstructorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

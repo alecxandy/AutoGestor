4e5afe645faabf641b62ee4f9eb500ee6b025004
alecxandy <alecxandy@gmail.com>
2023-10-24 23:14:15 -0300

    Microsservicos de pagamentos

---
+++ Back-end/ms_payment/src/test/java/https/github/com/alecxandy/AutoGestor/git/ms_payment/MsPaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package https.github.com.alecxandy.AutoGestor.git.ms_payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsPaymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

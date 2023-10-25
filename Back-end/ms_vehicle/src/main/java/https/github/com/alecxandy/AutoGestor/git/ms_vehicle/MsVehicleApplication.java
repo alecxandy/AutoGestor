f545bee7bda9af3c381852036267fa21ab8522e9
alecxandy <alecxandy@gmail.com>
2023-10-24 23:03:17 -0300

    Microsservicos de Veiculos

---
+++ Back-end/ms_vehicle/src/main/java/https/github/com/alecxandy/AutoGestor/git/ms_vehicle/MsVehicleApplication.java
@@ -0,0 +1,13 @@
+package https.github.com.alecxandy.AutoGestor.git.ms_vehicle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsVehicleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsVehicleApplication.class, args);
+	}
+
+}
---
+++ Back-end/ms_vehicle/src/test/java/https/github/com/alecxandy/AutoGestor/git/ms_vehicle/MsVehicleApplicationTests.java
@@ -0,0 +1,13 @@
+package https.github.com.alecxandy.AutoGestor.git.ms_vehicle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsVehicleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

0512aa82c573801eeb048def0404274753c8e7e2
alecxandy <alecxandy@gmail.com>
2023-10-24 23:08:10 -0300

    Microsservicos de Aulas

---
+++ Back-end/ms_classroom/src/main/java/https/github/com/alecxandy/AutoGestor/git/ms_classroom/MsClassroomApplication.java
@@ -0,0 +1,13 @@
+package https.github.com.alecxandy.AutoGestor.git.ms_classroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsClassroomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsClassroomApplication.class, args);
+	}
+
+}
